e14aec3e352ed200f63b38fabd6673f4a5c84d37
ryu068-sudo <ryu068@gmail.com>
2026-09-07 10:28:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "planner-v7";
+const CACHE = "planner-v8";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
